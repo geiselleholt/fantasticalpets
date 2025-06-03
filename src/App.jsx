8cf56f214b0b8e1186f4aa640f374d74553715f8
Geiselle Holt <geiselleholt@hotmail.com>
2025-06-02 21:28:44 -0600

    fixed erros, routes working

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import PetPage from "./pages/GamePage";
-import CreatePage from "./pages/RulesPage";
-import CollectionPage from "./pages/RulesPage";
-import SignInSignUpPage from "./pages/RulesPage";
+import PetPage from "./pages/PetPage";
+import CreatePage from "./pages/CreatePage";
+import CollectionPage from "./pages/CollectionPage";
+import SignInSignUpPage from "./pages/SignInSignUpPage";
 import NotFound from "./pages/NotFound";
 
 function App() {
