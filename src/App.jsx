cb2ef014117b2fb519217698b911b163582b3243
Geiselle Holt <geiselleholt@hotmail.com>
2025-06-12 23:30:46 -0600

    fixed protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
 import NotFound from "./pages/NotFound";
 import NavBar from "./components/NavBar";
+import ProtectedRoutes from "./components/ProtectedRoutes";
 
 function App() {
   return (
@@ -15,9 +16,11 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/pet" element={<PetPage />} />
-        <Route path="/create" element={<CreatePage />} />
-        <Route path="/collection" element={<CollectionPage />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/pet" element={<PetPage />} />
+          <Route path="/create" element={<CreatePage />} />
+          <Route path="/collection" element={<CollectionPage />} />
+        </Route>
         <Route path="/signIn" element={<SignInPage />} />
         <Route path="/signUp" element={<SignUpPage />} />
         <Route path="*" element={<NotFound />} />
--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import { useAuth } from "../context/authContext";
 import SignInPage from "../pages/SignInPage";
 
-export default function ProctectedRoutes() {
+export default function ProtectedRoutes() {
   const { cookies } = useAuth();
 
   return cookies.token ? <Outlet /> : <SignInPage />;
